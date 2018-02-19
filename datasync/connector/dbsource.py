e5d209efafc7f46307925f591804c3f76873b158
Karl Levik <karl.levik@diamond.ac.uk>
2018-02-19 03:42:57 +0000

    More methods defined in "interface"

--- datasync/connector/dbsource.py
+++ datasync/connector/dbsource.py
@@ -5,8 +5,17 @@ class DBSource:
     def extract_sessions_have_persons(self, greater_than):
         raise NotImplementedError
 
+    def extract_session_types(self):
+        raise NotImplementedError
+
     def extract_proposals(self):
         raise NotImplementedError
 
     def extract_sessions(self):
+        raise NotImplementedError
+
+    def extract_components(self):
+        raise NotImplementedError
+
+    def retrieve_persons_for_session(self, id):
         raise NotImplementedError
